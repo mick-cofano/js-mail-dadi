996eeee881b25828936004360db57a8e341f5b93
Michele Cofano <brutalmachinelol@gmail.com>
2020-12-15 18:02:24 +0100

    aggiunti risultati del giocatore e del pc

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 // Richiesta mail
-var mail = prompt('Ciao! Qual è la tua mail?')
-console.log('La sua mail è: ' + mail);
-
+var mail = prompt('Ciao! Qual è la tua mail?') //richiesta mail
 
 // mail giuste
-var mailAmmesse = ['alfredo@gmail.com', 'enrico@hotmail.com', 'michele@libero.it'];
+var mailAmmesse = ['alfredo@gmail.com', 'enrico@hotmail.com', 'michele@libero.it']; // mail ammesse
 if (mailAmmesse.indexOf(mail) !== -1) {
   alert('Congratulazioni, puoi accedere e giocare')
 } else {
@@ -12,7 +10,15 @@ if (mailAmmesse.indexOf(mail) !== -1) {
 }
 
 // gioco dei dadi random
+
 var player = [1,2,3,4,5,6]; // numeri da 1 a 6 per essere randomizzati
 var cpu = [1,2,3,4,5,6]; // numeri da 1 a 6 per essere randomizzati
 var playerResult;
 var cpuResult;
+
+
+//risultati
+playerResult = Math.floor((Math.random() * player.length) + 1); //risultato numero random da 1 a 6 player
+cpuResult = Math.floor((Math.random() * cpu.length) + 1); //risultato numero random da 1 a 6 CPU
+alert("Il risultato del tuo tiro è: " + playerResult);
+alert("Il risultato del tiro del PC è: " + cpuResult);
